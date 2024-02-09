363013e75681c421dac1c0594d2fc3f044547597
ShankarMadane8 <madaneshankar6@gmail.com>
2024-02-09 13:27:20 +0530

    add one line main file check it on jenkins

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
   public static void main(String[] args) {
 
 	System.out.println("stating project......");
+	System.out.println(" add new line test on jenkins......");
 	
 	
     }
